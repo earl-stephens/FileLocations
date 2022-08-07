12d0f8cf4f043872419cfdeaebd2996022afdf5f
earl-stephens <stephensearl@hotmail.com>
2022-08-06 22:07:14 -0600

    Add code to see if a file exists

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+
 public class App {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		
+		String fileLocation = "/Users/earltstephens/eclipse-workspace/UdemyPractice/FileLocations/test.txt";
+		
+		System.out.println(new File(fileLocation).exists());
 	}
 
 }
